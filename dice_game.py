4d318f8219c5376e8c8f3522f33f53288a2704c4
Tsurugi-T <tsuru9ta9@gmail.com>
2025-07-03 23:37:06 +0900

    Create dice_game.py
    greeting added

---
+++ dice_game.py
@@ -0,0 +1,15 @@
+import random
+
+# ユーザーに名前を尋ねる処理を追加
+name = input("What is your name? > ")
+print(f"Hello, {name}!")
+
+print("Rolling dice...")
+
+die1 = random.randint(1, 6)
+die2 = random.randint(1, 6)
+total = die1 + die2
+
+print(f"Die 1: {die1}")
+print(f"Die 2: {die2}")
+print(f"Total value: {total}")
